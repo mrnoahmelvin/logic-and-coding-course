3dd74779fdad97ab3fd438416900a2452f250d5f
mrnoahmelvin <54603638+mrnoahmelvin@users.noreply.github.com>
2019-09-18 15:14:08 -0700

    Add files via upload

---
+++ ForLoops/ForLoops.java
@@ -0,0 +1,34 @@
+//Java class ForLoops.java
+
+/**
+ * Skeleton code for the loops practice day.
+ * 
+ * @author Roy Chancellor
+ * @version 2/26/2019
+ */
+public class ForLoops
+{
+        public static void main ( String args[] ) {
+            
+            //int cube, a;
+            //cube =1;
+           // for (a=0; a<=10; a++){
+            //    cube= a*a*a;
+             //   System.out.println(a + "\t"+cube);
+            //int a;
+            //for(a=99; a>=1; a=a-2) {
+                
+            //System.out.println(a);
+            int n;
+            String a= "The quick brown fox jumped over the lazy dog";
+            n = a.length();
+            a.charAt(0);
+            for (int l = 0; l<a.length(); l++) {
+                System.out.println(a.charAt(l));
+                
+        }
+       
+    
+}
+}
+        
